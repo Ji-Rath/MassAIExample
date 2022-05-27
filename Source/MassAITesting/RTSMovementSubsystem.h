1b31971f8eb6adaa34a35bed08ee48c24a3f209b
Ji-Rath <garethaguilar@gmail.com>
2022-05-27 10:10:00 -0500

    Add experimental group movement to target location

---
+++ Source/MassAITesting/RTSMovementSubsystem.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "RTSMovementSubsystem.generated.h"
+
+/**
+ * Not currently used. Was simply a test to move entities to a target location
+ */
+UCLASS()
+class MASSAITESTING_API URTSMovementSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector TargetLocation;
+};
