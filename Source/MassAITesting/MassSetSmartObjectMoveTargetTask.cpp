597917fe1118759bdad1c4e3c83a8edca2aeee00
Ji-Rath <garethaguilar@gmail.com>
2022-05-28 16:00:42 -0500

    Add MassSetSmartObjectMoveTargetTask, used to take data from Smart Object User Fragment and transfer it to MoveTargetFragment. WIP

---
+++ Source/MassAITesting/MassSetSmartObjectMoveTargetTask.cpp
@@ -0,0 +1,70 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MassSetSmartObjectMoveTargetTask.h"
+
+#include "MassSmartObjectFragments.h"
+#include "MassStateTreeExecutionContext.h"
+
+bool FMassSetSmartObjectMoveTargetTask::Link(FStateTreeLinker& Linker)
+{
+	Linker.LinkExternalData(MoveTargetHandle);
+	Linker.LinkExternalData(TransformHandle);
+	Linker.LinkExternalData(SOUserHandle);
+	
+	return true;
+}
+
+EStateTreeRunStatus FMassSetSmartObjectMoveTargetTask::EnterState(FStateTreeExecutionContext& Context,
+	const EStateTreeStateChangeType ChangeType, const FStateTreeTransitionResult& Transition) const
+{
+	if (ChangeType != EStateTreeStateChangeType::Changed)
+	{
+		return EStateTreeRunStatus::Running;
+	}
+	
+	// Update MoveTarget location
+	//const FMassStateTreeExecutionContext& MassContext = static_cast<FMassStateTreeExecutionContext&>(Context);
+	FMassMoveTargetFragment& MoveTarget = Context.GetExternalData(MoveTargetHandle);
+	const FMassSmartObjectUserFragment& SOUserFragment = Context.GetExternalData(SOUserHandle);
+
+	if (!SOUserFragment.ClaimHandle.IsValid())
+		return EStateTreeRunStatus::Failed;
+
+	MoveTarget.Center = SOUserFragment.TargetLocation;
+	MoveTarget.Forward = SOUserFragment.TargetDirection;
+	MoveTarget.SlackRadius = 50.f;
+	UE_LOG(LogTemp,Error,TEXT("ENTER STATE!"));
+
+	return EStateTreeRunStatus::Running;
+}
+
+void FMassSetSmartObjectMoveTargetTask::ExitState(FStateTreeExecutionContext& Context,
+	const EStateTreeStateChangeType ChangeType, const FStateTreeTransitionResult& Transition) const
+{
+	if (ChangeType != EStateTreeStateChangeType::Changed)
+	{
+		return;
+	}
+	UE_LOG(LogTemp,Error,TEXT("EXITING STATE!"));
+}
+
+EStateTreeRunStatus FMassSetSmartObjectMoveTargetTask::Tick(FStateTreeExecutionContext& Context,
+                                                            const float DeltaTime) const
+{
+	// When entity reaches target, mark as complete
+	FMassMoveTargetFragment& MoveTarget = Context.GetExternalData(MoveTargetHandle);
+	const FTransform& Transform = Context.GetExternalData(TransformHandle).GetTransform();
+
+	MoveTarget.DistanceToGoal = (MoveTarget.Center - Transform.GetLocation()).Length();
+	
+
+	if (MoveTarget.DistanceToGoal <= MoveTarget.SlackRadius)
+	{
+		UE_LOG(LogTemp,Error,TEXT("REACHED TARGET!"));
+		return EStateTreeRunStatus::Succeeded;
+	}
+
+	UE_LOG(LogTemp,Error,TEXT("DISTANCE: %d"),MoveTarget.DistanceToGoal);
+	return EStateTreeRunStatus::Running;
+}
---
+++ Source/MassAITesting/MassSetSmartObjectMoveTargetTask.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MassCommonFragments.h"
+#include "MassNavigationFragments.h"
+#include "MassSmartObjectFragments.h"
+#include "MassStateTreeTypes.h"
+#include "SmartObjectTypes.h"
+#include "UObject/Object.h"
+#include "MassSetSmartObjectMoveTargetTask.generated.h"
+
+USTRUCT()
+struct MASSAITESTING_API FMassSetSmartObjectMoveTargetInstanceData
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+USTRUCT()
+struct MASSAITESTING_API FMassSetSmartObjectMoveTargetTask : public FMassStateTreeTaskBase
+{
+	GENERATED_BODY()
+	
+	virtual bool Link(FStateTreeLinker& Linker) override;
+	virtual const UStruct* GetInstanceDataType() const override { return FMassSetSmartObjectMoveTargetInstanceData::StaticStruct(); }
+
+	virtual EStateTreeRunStatus EnterState(FStateTreeExecutionContext& Context, const EStateTreeStateChangeType ChangeType, const FStateTreeTransitionResult& Transition) const override;
+	virtual void ExitState(FStateTreeExecutionContext& Context, const EStateTreeStateChangeType ChangeType, const FStateTreeTransitionResult& Transition) const override;
+	//virtual void StateCompleted(FStateTreeExecutionContext& Context, const EStateTreeRunStatus CompletionStatus, const FStateTreeHandle CompletedState) const {}
+	virtual EStateTreeRunStatus Tick(FStateTreeExecutionContext& Context, const float DeltaTime) const override;
+protected:
+	//TStateTreeInstanceDataPropertyHandle<float> DurationHandle;
+	TStateTreeExternalDataHandle<FMassMoveTargetFragment> MoveTargetHandle;
+	TStateTreeExternalDataHandle<FTransformFragment> TransformHandle;
+	TStateTreeExternalDataHandle<FMassSmartObjectUserFragment> SOUserHandle;
+};
