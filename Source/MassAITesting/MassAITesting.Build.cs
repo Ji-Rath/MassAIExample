bebacaf0207ea88580b91da581dfd94c6514ac74
Ji-Rath <garethaguilar@gmail.com>
2022-05-31 16:13:05 -0500

    Add MassSignals to public dependency module names

--- Source/MassAITesting/MassAITesting.Build.cs
+++ Source/MassAITesting/MassAITesting.Build.cs
@@ -10,6 +10,6 @@ public class MassAITesting : ModuleRules
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay",
 			"MassEntity", "MassCommon", "MassNavigation", "StructUtils", "MassMovement", "NavigationSystem",
-			"AIModule", "MassAIBehavior", "StateTreeModule", "GameplayTags", "SmartObjectsModule", "MassSmartObjects" });
+			"AIModule", "MassAIBehavior", "StateTreeModule", "GameplayTags", "SmartObjectsModule", "MassSmartObjects", "MassSignals" });
 	}
 }
