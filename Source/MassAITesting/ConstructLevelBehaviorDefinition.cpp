05f5d1e2eb4904d4f89edbc53ad2fa4a15815f0c
Ji-Rath <garethaguilar@gmail.com>
2022-06-06 14:29:29 -0500

    Add ConstructLevelBehaviorDefinition, a behavior that constructs an additional floor on the smart object 'house'

---
+++ Source/MassAITesting/ConstructLevelBehaviorDefinition.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ConstructLevelBehaviorDefinition.h"
+
+#include "MassSmartObjectFragments.h"
+#include "RTSAgentTrait.h"
+
+void UConstructLevelBehaviorDefinition::Activate(FMassCommandBuffer& CommandBuffer,
+                                                 const FMassBehaviorEntityContext& EntityContext) const
+{
+	Super::Activate(CommandBuffer, EntityContext);
+	// todo: check GetWorld() before accessing
+	FRTSBuildingFragment RTSBuildingFragment;
+	const UMassEntitySubsystem* EntitySubsystem = EntityContext.SmartObjectSubsystem.GetWorld()->GetSubsystem<UMassEntitySubsystem>();
+	const FMassSmartObjectUserFragment SOUser = EntitySubsystem->GetFragmentDataChecked<FMassSmartObjectUserFragment>(EntityContext.EntityView.GetEntity());
+	RTSBuildingFragment.BuildingClaimHandle = SOUser.ClaimHandle;
+	CommandBuffer.PushCommand(FCommandAddFragmentInstance(EntityContext.EntityView.GetEntity(), FConstStructView::Make(RTSBuildingFragment)));
+
+	CommandBuffer.AddTag<FRTSRequestResources>(EntityContext.EntityView.GetEntity());
+}
+
+void UConstructLevelBehaviorDefinition::Deactivate(FMassCommandBuffer& CommandBuffer,
+	const FMassBehaviorEntityContext& EntityContext) const
+{
+	Super::Deactivate(CommandBuffer, EntityContext);
+}
