21ece447513d4068ad1dd1ea0b06892dc6b4db66
Ji-Rath <garethaguilar@gmail.com>
2022-06-06 14:28:53 -0500

    Add MassStateTreeSmartObjectEvaluatorPlus, an alternative to MassStateTreeSmartObjectEvaluator that includes a filter to find filtered smart objects

---
+++ Source/MassAITesting/MassStateTreeSmartObjectEvaluatorPlus.cpp
@@ -0,0 +1,80 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MassStateTreeSmartObjectEvaluatorPlus.h"
+
+#include "MassAIBehaviorTypes.h"
+#include "MassCommonFragments.h"
+#include "MassSmartObjectFragments.h"
+#include "StateTreeExecutionContext.h"
+
+bool FMassStateTreeSmartObjectEvaluatorPlus::Link(FStateTreeLinker& Linker)
+{
+	Linker.LinkExternalData(SmartObjectSubsystemHandle);
+	Linker.LinkExternalData(MassSignalSubsystemHandle);
+	Linker.LinkExternalData(EntityTransformHandle);
+	Linker.LinkExternalData(SmartObjectUserHandle);
+
+	Linker.LinkInstanceDataProperty(SearchRequestResultHandle, STATETREE_INSTANCEDATA_PROPERTY(FMassStateTreeSmartObjectEvaluatorPlusInstanceData, SearchRequestResult));
+	Linker.LinkInstanceDataProperty(CandidatesFoundHandle, STATETREE_INSTANCEDATA_PROPERTY(FMassStateTreeSmartObjectEvaluatorPlusInstanceData, bCandidatesFound));
+	Linker.LinkInstanceDataProperty(FilterHandle, STATETREE_INSTANCEDATA_PROPERTY(FMassStateTreeSmartObjectEvaluatorPlusInstanceData, Filter));
+	Linker.LinkInstanceDataProperty(RangeHandle, STATETREE_INSTANCEDATA_PROPERTY(FMassStateTreeSmartObjectEvaluatorPlusInstanceData, Range));
+
+	return true;
+}
+
+void FMassStateTreeSmartObjectEvaluatorPlus::ExitState(FStateTreeExecutionContext& Context,
+	const EStateTreeStateChangeType ChangeType, const FStateTreeTransitionResult& Transition) const
+{
+	if (ChangeType != EStateTreeStateChangeType::Changed)
+	{
+		return;
+	}
+
+	Reset(Context);
+}
+
+void FMassStateTreeSmartObjectEvaluatorPlus::Evaluate(FStateTreeExecutionContext& Context,
+	const EStateTreeEvaluationType EvalType, const float DeltaTime) const
+{
+	USmartObjectSubsystem& SmartObjectSubsystem = Context.GetExternalData(SmartObjectSubsystemHandle);
+	FTransformFragment& TransformFragment = Context.GetExternalData(EntityTransformHandle);
+	UMassSignalSubsystem& SignalSubsystem = Context.GetExternalData(MassSignalSubsystemHandle);
+	FMassSmartObjectUserFragment& SOUser = Context.GetExternalData(SmartObjectUserHandle);
+
+	FMassSmartObjectRequestResult& RequestResult = Context.GetInstanceData(SearchRequestResultHandle);
+	FSmartObjectRequestFilter& Filter = Context.GetInstanceData(FilterHandle);
+	bool& CandidatesFound = Context.GetInstanceData(CandidatesFoundHandle);
+	float& Range = Context.GetInstanceData(RangeHandle);
+
+	const FTransform& Transform = TransformFragment.GetTransform();
+
+	bool bClaimed = SOUser.ClaimHandle.IsValid();
+
+	// Already claimed, nothing to do
+	if (bClaimed)
+	{
+		return;
+	}
+	
+	FSmartObjectRequest Request;
+	Request.Filter = Filter;
+	Request.QueryBox = FBox::BuildAABB(Transform.GetLocation(), FVector(Range));
+	FSmartObjectRequestResult Result = SmartObjectSubsystem.FindSmartObject(Request);
+	if (Result.IsValid())
+	{
+		RequestResult.Candidates[0] = FSmartObjectCandidate(Result.SmartObjectHandle, 0);
+		RequestResult.NumCandidates++;
+		RequestResult.bProcessed = true;
+		CandidatesFound = true;
+	}
+}
+
+void FMassStateTreeSmartObjectEvaluatorPlus::Reset(FStateTreeExecutionContext& Context) const
+{
+	bool& bCandidatesFound = Context.GetInstanceData(CandidatesFoundHandle);
+	FMassSmartObjectRequestResult& RequestResult = Context.GetInstanceData(SearchRequestResultHandle);
+	bCandidatesFound = false;
+	RequestResult.bProcessed = false;
+	RequestResult.NumCandidates = 0;
+}
---
+++ Source/MassAITesting/MassStateTreeSmartObjectEvaluatorPlus.h
@@ -0,0 +1,57 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "MassSignalSubsystem.h"
+#include "MassSmartObjectRequest.h"
+#include "MassStateTreeTypes.h"
+#include "SmartObjectSubsystem.h"
+#include "MassStateTreeSmartObjectEvaluatorPlus.generated.h"
+
+struct FMassSmartObjectUserFragment;
+struct FTransformFragment;
+
+USTRUCT()
+struct MASSAITESTING_API FMassStateTreeSmartObjectEvaluatorPlusInstanceData
+{
+	GENERATED_BODY()
+
+	/** The identifier of the search request send by the evaluator to find candidates */
+	UPROPERTY(EditAnywhere, Category = Output)
+	FMassSmartObjectRequestResult SearchRequestResult;
+
+	/** Indicates that the result of the candidates search is ready and contains some candidates */
+	UPROPERTY(EditAnywhere, Category = Output)
+	bool bCandidatesFound = false;
+
+	UPROPERTY(EditAnywhere, Category = Parameter)
+	FSmartObjectRequestFilter Filter = FSmartObjectRequestFilter();
+
+	UPROPERTY(EditAnywhere, Category = Parameter)
+	float Range = 5000.f;
+};
+
+/**
+ * 
+ */
+USTRUCT()
+struct MASSAITESTING_API FMassStateTreeSmartObjectEvaluatorPlus : public FMassStateTreeEvaluatorBase
+{
+	GENERATED_BODY()
+
+	virtual bool Link(FStateTreeLinker& Linker) override;
+	virtual const UStruct* GetInstanceDataType() const override { return FMassStateTreeSmartObjectEvaluatorPlusInstanceData::StaticStruct(); }
+	virtual void ExitState(FStateTreeExecutionContext& Context, const EStateTreeStateChangeType ChangeType, const FStateTreeTransitionResult& Transition) const override;
+	virtual void Evaluate(FStateTreeExecutionContext& Context, const EStateTreeEvaluationType EvalType, const float DeltaTime) const override;
+	void Reset(FStateTreeExecutionContext& Context) const;
+
+	TStateTreeInstanceDataPropertyHandle<FMassSmartObjectRequestResult> SearchRequestResultHandle;
+	TStateTreeInstanceDataPropertyHandle<bool> CandidatesFoundHandle;
+	TStateTreeInstanceDataPropertyHandle<FSmartObjectRequestFilter> FilterHandle;
+	TStateTreeInstanceDataPropertyHandle<float> RangeHandle;
+	
+	TStateTreeExternalDataHandle<USmartObjectSubsystem> SmartObjectSubsystemHandle;
+	TStateTreeExternalDataHandle<UMassSignalSubsystem> MassSignalSubsystemHandle;
+	TStateTreeExternalDataHandle<FTransformFragment> EntityTransformHandle;
+	TStateTreeExternalDataHandle<FMassSmartObjectUserFragment> SmartObjectUserHandle;
+};
