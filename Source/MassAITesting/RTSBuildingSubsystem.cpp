d09ab66c1a57f82c8c01a20582e3022407345ae5
Ji-Rath <garethaguilar@gmail.com>
2022-06-15 13:29:38 -0500

    Add RTSBuildingSubsystem to handle building tasks and distributing to entities

---
+++ Source/MassAITesting/RTSBuildingSubsystem.cpp
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RTSBuildingSubsystem.h"
+
+void URTSBuildingSubsystem::AddBuilding(const FSmartObjectHandle& BuildingRequest, int Floors)
+{
+	QueuedBuildings.Emplace(FBuilding(BuildingRequest, Floors));
+}
+
+void URTSBuildingSubsystem::ClaimFloor(FSmartObjectHandle& Building)
+{
+	if (QueuedBuildings.Num() > 0)
+	{
+		FBuilding& BuildStruct = QueuedBuildings[0];
+		Building = BuildStruct.BuildingRequest;
+		BuildStruct.FloorsNeeded--;
+		UE_LOG(LogTemp, Error, TEXT("Building ID: %s | Floors remaining: %d"), *LexToString(BuildStruct.BuildingRequest), BuildStruct.FloorsNeeded)
+		if (BuildStruct.FloorsNeeded <= 0)
+			QueuedBuildings.RemoveAt(0);
+	}
+}
---
+++ Source/MassAITesting/RTSBuildingSubsystem.h
@@ -0,0 +1,59 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SmartObjectSubsystem.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "RTSBuildingSubsystem.generated.h"
+
+USTRUCT()
+struct MASSAITESTING_API FBuilding
+{
+	GENERATED_BODY()
+	
+	UPROPERTY()
+	FSmartObjectHandle BuildingRequest;
+
+	UPROPERTY()
+	int FloorsNeeded = 1;
+
+	FBuilding() {}
+
+	FBuilding(const FSmartObjectHandle& BuildingRequest, int FloorsNeeded)
+	{
+		this->BuildingRequest = BuildingRequest;
+		this->FloorsNeeded = FloorsNeeded;
+	}
+
+	bool operator == (const FBuilding& OtherBuilding) const
+	{
+		return OtherBuilding.BuildingRequest == this->BuildingRequest;
+	}
+};
+/**
+ * Building subsystem used to queue buildings and floors for construction for RTS Agents
+ */
+UCLASS()
+class MASSAITESTING_API URTSBuildingSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	TArray<FBuilding> QueuedBuildings;
+
+	/**
+	 * @brief Add a building to the subsystem for entities to build
+	 * @param BuildingRequest Smart Object Handle of the building
+	 * @param Floors The amount of floors to build
+	 */
+	UFUNCTION(BlueprintCallable)
+	void AddBuilding(const FSmartObjectHandle& BuildingRequest, int Floors = 1);
+
+	/**
+	 * @brief Claim a 'random' building that needs construction of a floor
+	 * @param Building OUT Smart Object handle used for completing the construction when resources are collected
+	 */
+	UFUNCTION()
+	void ClaimFloor(FSmartObjectHandle& Building);
+};
