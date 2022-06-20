8e894ddf4228bc0f605a1a20d8ae0a1d4276f488
Ji-Rath <garethaguilar@gmail.com>
2022-06-20 13:23:12 -0500

    Begin implementing droppable items, their position stored in a hash grid

---
+++ Source/MassAITesting/RTSItemTrait.cpp
@@ -0,0 +1,118 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RTSItemTrait.h"
+
+#include "RTSBuildingSubsystem.h"
+#include "MassCommonFragments.h"
+#include "MassEntityTemplateRegistry.h"
+#include "Engine/World.h"
+
+void URTSItemTrait::BuildTemplate(FMassEntityTemplateBuildContext& BuildContext, UWorld& World) const
+{
+	BuildContext.AddFragment<FItemFragment>();
+}
+
+UItemInitializerProcessor::UItemInitializerProcessor()
+{
+	ObservedType = FItemFragment::StaticStruct();
+	Operation = EMassObservedOperation::Add;
+}
+
+void UItemInitializerProcessor::ConfigureQueries()
+{
+	EntityQuery.AddRequirement<FItemFragment>(EMassFragmentAccess::None);
+	EntityQuery.AddRequirement<FTransformFragment>(EMassFragmentAccess::ReadOnly);
+}
+
+void UItemInitializerProcessor::Initialize(UObject& Owner)
+{
+	BuildingSubsystem = UWorld::GetSubsystem<URTSBuildingSubsystem>(Owner.GetWorld());
+}
+
+UItemRemoverProcessor::UItemRemoverProcessor()
+{
+	ObservedType = FItemFragment::StaticStruct();
+	Operation = EMassObservedOperation::Remove;
+}
+
+void UItemRemoverProcessor::Execute(UMassEntitySubsystem& EntitySubsystem, FMassExecutionContext& Context)
+{
+	EntityQuery.ParallelForEachEntityChunk(EntitySubsystem, Context, [this](FMassExecutionContext& Context)
+	{
+		TArrayView<FItemFragment> ItemFragments = Context.GetMutableFragmentView<FItemFragment>();
+		
+		for (int32 EntityIndex = 0; EntityIndex < Context.GetNumEntities(); ++EntityIndex)
+		{
+			BuildingSubsystem->ItemHashGrid.RemovePoint(Context.GetEntity(EntityIndex), ItemFragments[EntityIndex].OldLocation);
+		}
+	});
+}
+
+void UItemRemoverProcessor::ConfigureQueries()
+{
+	EntityQuery.AddRequirement<FItemFragment>(EMassFragmentAccess::None);
+}
+
+void UItemRemoverProcessor::Initialize(UObject& Owner)
+{
+	BuildingSubsystem = UWorld::GetSubsystem<URTSBuildingSubsystem>(Owner.GetWorld());
+}
+
+UItemProcessor::UItemProcessor()
+{
+	
+}
+
+void UItemProcessor::Execute(UMassEntitySubsystem& EntitySubsystem, FMassExecutionContext& Context)
+{
+	EntityQuery.ParallelForEachEntityChunk(EntitySubsystem, Context, [this](FMassExecutionContext& Context)
+	{
+		TConstArrayView<FTransformFragment> Transforms = Context.GetFragmentView<FTransformFragment>();
+		TArrayView<FItemFragment> ItemFragments = Context.GetMutableFragmentView<FItemFragment>();
+		
+		for (int32 EntityIndex = 0; EntityIndex < Context.GetNumEntities(); ++EntityIndex)
+		{
+			FItemFragment& Item = ItemFragments[EntityIndex];
+			const FVector& Location = Transforms[EntityIndex].GetTransform().GetLocation();
+			const FVector2D Location2D(Location.X, Location.Y);
+			
+			BuildingSubsystem->ItemHashGrid.UpdatePoint(Context.GetEntity(EntityIndex), Item.OldLocation, Location2D);
+			Item.OldLocation = Location2D;
+		}
+	});
+}
+
+void UItemProcessor::ConfigureQueries()
+{
+	EntityQuery.AddRequirement<FItemFragment>(EMassFragmentAccess::ReadWrite);
+	EntityQuery.AddTagRequirement<FItemAddedToGrid>(EMassFragmentPresence::All);
+	EntityQuery.AddRequirement<FTransformFragment>(EMassFragmentAccess::ReadOnly);
+}
+
+void UItemProcessor::Initialize(UObject& Owner)
+{
+	BuildingSubsystem = UWorld::GetSubsystem<URTSBuildingSubsystem>(Owner.GetWorld());
+}
+
+void UItemInitializerProcessor::Execute(UMassEntitySubsystem& EntitySubsystem, FMassExecutionContext& Context)
+{
+	EntityQuery.ParallelForEachEntityChunk(EntitySubsystem, Context, [this](FMassExecutionContext& Context)
+	{
+		TConstArrayView<FTransformFragment> Transforms = Context.GetFragmentView<FTransformFragment>();
+		TArrayView<FItemFragment> ItemFragments = Context.GetMutableFragmentView<FItemFragment>();
+		
+		for (int32 EntityIndex = 0; EntityIndex < Context.GetNumEntities(); ++EntityIndex)
+		{
+			FItemFragment& Item = ItemFragments[EntityIndex];
+			const FTransform& Transform = Transforms[EntityIndex].GetTransform();
+			const FVector& Location = Transform.GetLocation();
+			const FVector2D Location2D(Location.X, Location.Y);
+			
+			BuildingSubsystem->ItemHashGrid.InsertPoint(Context.GetEntity(EntityIndex), Location2D);
+			Item.OldLocation = Location2D;
+			
+			Context.Defer().AddTag<FItemAddedToGrid>(Context.GetEntity(EntityIndex));
+		}
+	});
+}
---
+++ Source/MassAITesting/RTSItemTrait.h
@@ -0,0 +1,96 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RTSAgentTrait.h"
+#include "MassEntityTraitBase.h"
+#include "MassEntityTypes.h"
+#include "MassObserverProcessor.h"
+#include "RTSItemTrait.generated.h"
+
+struct FMassEntityTemplateBuildContext;
+/**
+ * 
+ */
+UCLASS()
+class MASSAITESTING_API URTSItemTrait : public UMassEntityTraitBase
+{
+	GENERATED_BODY()
+
+	virtual void BuildTemplate(FMassEntityTemplateBuildContext& BuildContext, UWorld& World) const override;
+};
+
+USTRUCT()
+struct MASSAITESTING_API FItemFragment : public FMassFragment
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	TEnumAsByte<EResourceType> ItemType;
+
+	UPROPERTY()
+	FVector2D OldLocation;
+
+	UPROPERTY()
+	bool bClaimed = false;
+};
+
+USTRUCT()
+struct MASSAITESTING_API FItemAddedToGrid : public FMassTag
+{
+	GENERATED_BODY()
+};
+
+UCLASS()
+class MASSAITESTING_API UItemInitializerProcessor : public UMassObserverProcessor
+{
+	GENERATED_BODY()
+
+	UItemInitializerProcessor();
+
+public:
+	virtual void Execute(UMassEntitySubsystem& EntitySubsystem, FMassExecutionContext& Context) override;
+	virtual void ConfigureQueries() override;
+	virtual void Initialize(UObject& Owner) override;
+
+	FMassEntityQuery EntityQuery;
+
+	UPROPERTY()
+	URTSBuildingSubsystem* BuildingSubsystem;
+};
+
+UCLASS()
+class MASSAITESTING_API UItemRemoverProcessor : public UMassObserverProcessor
+{
+	GENERATED_BODY()
+
+	UItemRemoverProcessor();
+
+public:
+	virtual void Execute(UMassEntitySubsystem& EntitySubsystem, FMassExecutionContext& Context) override;
+	virtual void ConfigureQueries() override;
+	virtual void Initialize(UObject& Owner) override;
+
+	FMassEntityQuery EntityQuery;
+
+	UPROPERTY()
+	URTSBuildingSubsystem* BuildingSubsystem;
+};
+
+UCLASS()
+class MASSAITESTING_API UItemProcessor : public UMassProcessor
+{
+	GENERATED_BODY()
+	
+	UItemProcessor();
+public:
+	virtual void Execute(UMassEntitySubsystem& EntitySubsystem, FMassExecutionContext& Context) override;
+	virtual void ConfigureQueries() override;
+	virtual void Initialize(UObject& Owner) override;
+
+	FMassEntityQuery EntityQuery;
+
+	UPROPERTY()
+	URTSBuildingSubsystem* BuildingSubsystem;
+};
