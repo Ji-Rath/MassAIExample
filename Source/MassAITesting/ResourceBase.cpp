a61a35d023d07fe22d1f56b3c3d89b009b80f074
Ji-Rath <garethaguilar@gmail.com>
2022-06-21 13:05:16 -0500

    Add ResourceBase to add resource smart object to RTSBuildingSubsystem

---
+++ Source/MassAITesting/ResourceBase.cpp
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ResourceBase.h"
+
+#include "RTSBuildingSubsystem.h"
+
+
+// Sets default values
+AResourceBase::AResourceBase()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SmartObjectComp = CreateDefaultSubobject<USmartObjectComponent>(TEXT("Smart Object Component"));
+	SetRootComponent(SmartObjectComp);
+}
+
+// Called when the game starts or when spawned
+void AResourceBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AResourceBase::AddToResourceQueue()
+{
+	FSmartObjectHandle SOHandle = SmartObjectComp->GetRegisteredHandle();
+	GetWorld()->GetSubsystem<URTSBuildingSubsystem>()->AddResourceQueue(SOHandle);
+}
+
+// Called every frame
+void AResourceBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/MassAITesting/ResourceBase.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SmartObjectComponent.h"
+#include "GameFramework/Actor.h"
+#include "BuildingBase.generated.h"
+
+UCLASS()
+class MASSAITESTING_API AResourceBase : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AResourceBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void AddToResourceQueue();
+	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	USmartObjectComponent* SmartObjectComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int Floors = 1;
+};
