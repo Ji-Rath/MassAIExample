064d5f18f744ebc12d165d226926f97912a4da44
Ji-Rath <garethaguilar@gmail.com>
2022-07-29 13:42:27 -0500

    Add GotoRandomLocationTask to move to a random location when idle

---
+++ Source/MassAITesting/StateTree/Tasks/MassStateTreeGotoRandomLocationTask.cpp
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MassStateTreeGotoRandomLocationTask.h"
+
+#include "MassCommonFragments.h"
+#include "MassSetSmartObjectMoveTargetTask.h"
+#include "MassAITesting/Mass/RTSItemTrait.h"
+
+bool FMassStateTreeGotoRandomLocationTask::Link(FStateTreeLinker& Linker)
+{
+	Linker.LinkInstanceDataProperty(RadiusHandle, STATETREE_INSTANCEDATA_PROPERTY(FMassStateTreeGotoRandomLocationTaskInstanceData, Radius));
+	
+	Linker.LinkExternalData(TransformHandle);
+	Linker.LinkExternalData(MoveTargetHandle);
+	return true;
+}
+
+EStateTreeRunStatus FMassStateTreeGotoRandomLocationTask::EnterState(FStateTreeExecutionContext& Context,
+	const EStateTreeStateChangeType ChangeType, const FStateTreeTransitionResult& Transition) const
+{
+	FMassMoveTargetFragment& MoveTargetFragment = Context.GetExternalData(MoveTargetHandle);
+	const FTransform& Transform = Context.GetExternalData(TransformHandle).GetTransform();
+	const float& Radius = Context.GetInstanceData(RadiusHandle);
+	FVector RandomLocation = FVector(FMath::RandRange(-Radius, Radius), FMath::RandRange(-Radius, Radius), 0.f);
+	RandomLocation += Transform.GetLocation();
+
+	MoveTargetFragment.CreateNewAction(EMassMovementAction::Move, *Context.GetWorld());
+	MoveTargetFragment.Center = RandomLocation;
+	
+	return EStateTreeRunStatus::Running;
+}
+
+EStateTreeRunStatus FMassStateTreeGotoRandomLocationTask::Tick(FStateTreeExecutionContext& Context,
+	const float DeltaTime) const
+{
+	FMassMoveTargetFragment& MoveTarget = Context.GetExternalData(MoveTargetHandle);
+
+	if (MoveTarget.DistanceToGoal <= MoveTarget.SlackRadius)
+	{
+		MoveTarget.CreateNewAction(EMassMovementAction::Stand, *Context.GetWorld());
+		return EStateTreeRunStatus::Succeeded;
+	}
+	
+	return EStateTreeRunStatus::Running;
+}
---
+++ Source/MassAITesting/StateTree/Tasks/MassStateTreeGotoRandomLocationTask.h
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "StateTreeExecutionContext.h"
+#include "MassStateTreeTypes.h"
+#include "MassEntityTypes.h"
+#include "MassStateTreeGotoRandomLocationTask.generated.h"
+
+struct FMassMoveTargetFragment;
+struct FTransformFragment;
+
+class URTSBuildingSubsystem;
+USTRUCT()
+struct FMassStateTreeGotoRandomLocationTaskInstanceData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = Parameter)
+	float Radius = 100.f;
+};
+
+/**
+ * 
+ */
+USTRUCT()
+struct MASSAITESTING_API FMassStateTreeGotoRandomLocationTask : public FMassStateTreeTaskBase
+{
+	GENERATED_BODY()
+	
+	virtual bool Link(FStateTreeLinker& Linker) override;
+	virtual const UStruct* GetInstanceDataType() const override { return FMassStateTreeGotoRandomLocationTaskInstanceData::StaticStruct(); }
+	virtual EStateTreeRunStatus EnterState(FStateTreeExecutionContext& Context, const EStateTreeStateChangeType ChangeType, const FStateTreeTransitionResult& Transition) const override;
+	virtual EStateTreeRunStatus Tick(FStateTreeExecutionContext& Context, const float DeltaTime) const override;
+
+	TStateTreeInstanceDataPropertyHandle<float> RadiusHandle;
+
+	TStateTreeExternalDataHandle<FMassMoveTargetFragment> MoveTargetHandle;
+	TStateTreeExternalDataHandle<FTransformFragment> TransformHandle;
+	TStateTreeExternalDataHandle<URTSBuildingSubsystem> BuildingSubsystemHandle;
+};
