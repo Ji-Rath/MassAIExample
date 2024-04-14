7a38239146742901cfce96a60a47af05e09eb84c
Gareth Aguilar <garethaguilar@gmail.com>
2024-04-14 11:34:47 -0500

    Push experimental changes for SmartObjectAI plugin

---
+++ Plugins/MassSmartObjectAI/Source/MassSmartObjectAI/Private/Processors/TickSTProcessor.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MassProcessor.h"
+#include "TickSTProcessor.generated.h"
+
+/**
+ * Processor whos function is to tick the state tree at certain intervals.
+ * This may fluctuate due to distance, LOD, or other factors.
+ */
+UCLASS()
+class MASSSMARTOBJECTAI_API UTickSTProcessor : public UMassProcessor
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void ConfigureQueries() override;
+	virtual void Execute(FMassEntityManager& EntityManager, FMassExecutionContext& Context) override;
+
+	FMassEntityQuery EntityQuery;
+};
