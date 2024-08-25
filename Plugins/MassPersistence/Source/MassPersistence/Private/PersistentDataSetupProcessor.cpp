41f3ee22ef60959ef86a590c5ddf9de04d0940b1
JiRath <garethaguilar@gmail.com>
2024-08-25 15:23:08 -0500

    Optimized save/load operations so they are now async calls Batch cleanup operations to hopefully run quicker

--- Plugins/MassPersistence/Source/MassPersistence/Private/PersistentDataSetupProcessor.cpp
+++ Plugins/MassPersistence/Source/MassPersistence/Private/PersistentDataSetupProcessor.cpp
@@ -60,11 +60,18 @@ void UPersistentDataDestructorProcessor::Execute(FMassEntityManager& EntityManag
 	EntityQuery.ForEachEntityChunk(EntityManager, Context, [this](FMassExecutionContext& Context)
 	{
 		auto& PersistentDataSubsystem = Context.GetMutableSubsystemChecked<UMassPersistentDataSubsystem>();
-		
+
+		TSet<FMassEntityHandle> EntitiesToRemove;
 		const int32 NumEntities = Context.GetNumEntities();
 		for (int EntityIdx = 0; EntityIdx < NumEntities; EntityIdx++)
 		{
-			PersistentDataSubsystem.ManagedEntities.RemoveSwap(Context.GetEntity(EntityIdx));
+			EntitiesToRemove.Add(Context.GetEntity(EntityIdx));
 		}
+		
+		PersistentDataSubsystem.ManagedEntities.RemoveAllSwap([&EntitiesToRemove](FMassEntityHandle& Entity)
+		{
+			return EntitiesToRemove.Contains(Entity);
+		});
 	});
+	
 }
